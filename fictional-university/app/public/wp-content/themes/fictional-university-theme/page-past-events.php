5984dbfc3f2952d82d3e2dc088c3fd39f3a0baa0
Syafiq <syafiqabdul@gmail.com>
2022-10-24 00:20:49 +0800

    1.Create pagination for past event page
    Custom query page for past events page
    
    2.Highlight header link when inside past event

--- fictional-university/app/public/wp-content/themes/fictional-university-theme/page-past-events.php
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/page-past-events.php
@@ -18,6 +18,7 @@ get_header(); ?>
 
         $today = date('Ymd');
         $pastEvents = new WP_Query (array(
+            'paged' => get_query_var('paged', 1),
             'post_type' => 'event',
             'meta_key' => 'event_date',
             'meta_query' => array(
@@ -52,7 +53,9 @@ get_header(); ?>
             </div>
 
       <?php }
-      echo paginate_links();
+      echo paginate_links(array(
+        'total' => $pastEvents -> max_num_pages
+      ));
       ?>
 
 <?php 
